bb5b0e26e96a2862c6ffe4e114d3d33f31268cdb
GaozyDev <670453367@qq.com>
2020-04-20 22:40:51 +0800

    创建登录界面

--- app/src/main/java/com/gzy/timer/ui/main/MainActivity.java
+++ app/src/main/java/com/gzy/timer/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.gzy.timer;
+package com.gzy.timer.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.gzy.timer.R;
 
 public class MainActivity extends AppCompatActivity {
 
